dc460a1c32ba50ed9a8d820007c8fbf68c1025f6
vishalsanfran <vkhawarey@maprtech.com>
2017-09-28 06:58:45 -0700

    consume rest svc

---
+++ consume_rest/src/main/java/hello/Application.java
@@ -0,0 +1,16 @@
+package hello;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.client.RestTemplate;
+
+public class Application {
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args) {
+        RestTemplate restTemplate = new RestTemplate();
+        Quote quote = restTemplate.getForObject(
+            "http://gturnquist-quoters.cfapps.io/api/random", Quote.class);
+        log.info("quote " + quote);
+    }
+}
